673d7c4bbf3bd232132482ac3a46e9ad3f5afad2
burningtail <luis.mancas@yahoo.com>
2022-10-24 19:38:25 +0300

    testAdd project files.

---
+++ Data/Banica_Luis_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Banica_Luis_Lab2.Models;
+
+namespace Banica_Luis_Lab2.Data
+{
+    public class Banica_Luis_Lab2Context : DbContext
+    {
+        public Banica_Luis_Lab2Context (DbContextOptions<Banica_Luis_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Banica_Luis_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
